f4a4a246fb167e4b49b4cec6699fceb1369e96c5
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-12 14:18:54 +0200

    Add internal and external versions of score

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,21 +1,23 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     var word = MutableLiveData<String>()
-    var score = MutableLiveData<Int>()
-
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
 
     private fun resetList() {
@@ -54,12 +56,12 @@ class GameViewModel : ViewModel() {
     }
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 }
