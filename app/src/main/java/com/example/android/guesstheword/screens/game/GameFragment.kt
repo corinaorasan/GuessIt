c2448ef1e8e2f10bad7b03530251570c8fd09d0c
Orășan Corina <62331686+corinaorasan@users.noreply.github.com>
2021-03-15 14:20:02 +0200

    Add a view model factory (#6)
    * Add ScoreViewModel class
    
    * Add ScoreViewModelFactory class
    
    * Construct ViewModel and ViewModelFactory
    
    * Use ViewModelFactory to create ScoreViewModel
    
    * Return the ScoreViewModel
    
    * Add LiveData for the score
    
    * Add eventPlayAgain w/ LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -80,7 +80,6 @@ class GameFragment : Fragment() {
 
         viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
             binding.timerText.text = DateUtils.formatElapsedTime(newTime)
-
         })
     }
 
