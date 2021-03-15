6bd115a61beb2699c604c484b77da98642c2020f
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-03-15 17:25:03 +0200

    Set text attribute for word_text using LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -53,6 +53,7 @@ class GameFragment : Fragment() {
 
         binding.gameViewModel=viewModel
         binding.setLifecycleOwner(this)
+
         setLiveDataObservationRelationship()
         setEventGameFinish()
 
@@ -63,10 +64,6 @@ class GameFragment : Fragment() {
     private fun setLiveDataObservationRelationship() {
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
-        })
-
-        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
-            binding.wordText.text = newWord
         })
 
         viewModel.currentTime.observe(viewLifecycleOwner, Observer { newTime ->
